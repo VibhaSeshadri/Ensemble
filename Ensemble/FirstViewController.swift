72d6c7612f1c595d06480d48c1974b169ea8e989
VibhaSeshadri <vibhaseshadri@gmail.com>
2016-06-20 19:31:58 -0700

    Commit to sync
    Committing to sync to online version

---
+++ Ensemble/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Ensemble
+//
+//  Created by Seshadri Family on 6/20/16.
+//  Copyright © 2016 Vibha&Meera. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
